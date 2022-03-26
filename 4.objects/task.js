b54ee2697a8262d1cdabbbb65b709863c56d68ef
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-03-26 14:21:09 +0300

    tasks ready

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let result = 0;
+
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  } result = sum / this.marks.length;
+  return result;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
