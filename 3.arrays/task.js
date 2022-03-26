c321321a1526a06bfc4f92bc87959f085a0d4cd3
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-03-26 14:12:39 +0300

    tasks ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item, i) => item === arr2[i]);
+
+  } else result = false;
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((number) => number > 0 && number % 3 === 0).map((number) => number * 10);
 
   return resultArr; // array
 }
