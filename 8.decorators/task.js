0bc8eacf17e7d23ddda70657ded7d471c387f3c9
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2022-04-04 17:41:18 +0300

    Task ready

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,72 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cashe = [];
+  let counter =0;
+
+  function wrapper(...args) {
+
+    const hash = args.join(',');
+
+    if (hash in cashe) {
+      return "Из кэша: " + cashe[hash];
+  }
+  if (counter >= 5) {
+      delete cashe[Object.keys(cashe)[0]];
+  }
+  counter++;
+  cashe[hash] = func(...args);
+  return "Вычисляем: " + cashe[hash];
+}
+
+return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, ms) {
+  let isThrottled = false;
+	let timeout;
+
+	function wrapper(...args) {
+		clearTimeout(timeout);
+
+		timeout = setTimeout(() => {
+			isThrottled = false;
+			func.apply(this, args);
+		}, ms);
+
+		if (isThrottled) return;
+
+		func.apply(this, args);
+		isThrottled = true;
+	};
+	return wrapper;
+  
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let isThrottled = false;
+	let timeout;
+	wrapper.count = 0;
+
+	function wrapper(...args) {
+
+		clearTimeout(timeout);
+
+		timeout = setTimeout(() => {
+			isThrottled = false;
+			func.apply(this, args);
+			wrapper.count++;
+		}, ms);
+
+		if (isThrottled) {
+			return;
+		};
+
+		func.apply(this, args);
+		wrapper.count++;
+		isThrottled = true;
+
+		console.log(wrapper.count);
+	};
+	return wrapper;
 }
